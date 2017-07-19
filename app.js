5c363218d78ccab1ab64cf5fd51cdfcc112212b3
Scott Schaefer <scottschaef@gmail.com>
2017-07-19 19:34:01 -0400

    Created grocery list with items in react pattern

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor (props) {
+    super (props)
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onHoverItem() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render () {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <ul>
+        <li style={style} onMouseOver={this.onHoverItem.bind(this)}>{this.props.item}</li>
+      </ul>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <div>
+    {props.items.map(item => <GroceryListItem item={item} />)}
+  </div>
+);
+
+ReactDOM.render(<GroceryList items={['eggs', 'bread']} />, document.getElementById("app"));
